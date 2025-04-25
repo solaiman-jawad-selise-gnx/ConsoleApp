37ffb39f8202a57e086ab7aa3ff6387469cefe33
Solaiman Jawad <solaiman.jawad@selisegroup.com>
2025-04-25 13:13:09 +0600

    Making Calculator App OOP + using some concepts from 2nd and 3rd tut series

---
+++ Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Application
+    {
+
+        static void Main(string[] args)
+        {
+            Operand firstNum = new Operand();
+            Operand secondNum = new Operand();
+            Calculator op = new Calculator();
+
+            firstNum.OperandInputedCorrectly += op.OnCorrectOperandInputed;
+            secondNum.OperandInputedCorrectly += op.OnCorrectOperandInputed;
+            
+            firstNum.ReadOperandFromIo();
+            secondNum.ReadOperandFromIo();
+            
+            Console.WriteLine("\nYour Result is: " + op.Result);
+        }
+    }
+}
